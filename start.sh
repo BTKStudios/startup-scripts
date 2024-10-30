5c101bb4bed8087c27fe31b43111f27218e20ce5
fleroviums <93944186+fleroviums@users.noreply.github.com>
2024-10-30 14:10:00 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -40,17 +40,21 @@ else
 	echo -e "No GitHub Repo was provided. Skipping git monitor..."
 fi
 
+if [ -e pip ]; then
+	echo -e "Pipe already created. Deleting..."
+	rm pipe
+fi
+
 mkfifo pipe
 
-echo -e "Creating pipe..."
-cat > pipe &
-CAT_PID=$!
-
 echo -e "Starting Minecraft Server..."
-java -Xmx$JVM_RAM -Xms$JVM_RAM -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Djdk.console=java.base -jar server.jar nogui < pipe
+java -Xmx$JVM_RAM -Xms$JVM_RAM -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Djdk.console=java.base -jar server.jar nogui < pipe &
+SERVER_PID=$!
 
-kill $CAT_PID
-rm pipe
+while [ $SERVER_PID ] do
+	cat > pipe
+done
+
 
 echo -e "Minecraft server exited."
 
