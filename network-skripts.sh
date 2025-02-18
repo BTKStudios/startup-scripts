80e90076bfc1630300867610387e7bf8f053d7b7
fleroviums <93944186+fleroviums@users.noreply.github.com>
2025-02-18 13:31:10 +0000

    Update network-skripts.sh

--- network-skripts.sh
+++ network-skripts.sh
@@ -18,7 +18,7 @@ LAST_COMMIT_FILE="/tmp/last_commit_hash"
 LOG_DIR="/home/container/git-monitor"
 mkdir -p "$LOG_DIR"
 
-eval "$(ssh-agent -s)" > "$LOG_DIR/log.txt"
+eval "$(ssh-agent -s)" > "$LOG_DIR/network-skripts-log.txt"
 ssh-add /home/container/ssh/id-rsa >> "$LOG_DIR/network-skripts-log.txt" 2>&1
 
 # Get the last commit hash from the repository
@@ -31,24 +31,24 @@ LAST_COMMIT=$(git rev-parse HEAD)
 
 while true; do
     # Fetch the latest changes for the specific branch
-   git fetch origin "$BRANCH" >> "$LOG_DIR/log.txt" 2>&1
+   git fetch origin "$BRANCH" >> "$LOG_DIR/network-skripts-log.txt" 2>&1
 
     # Get the latest commit hash on the specified branch
     CURRENT_COMMIT=$(git rev-parse origin/"$BRANCH")
 
     # Compare with the last known commit
     if [ "$CURRENT_COMMIT" != "$LAST_COMMIT" ]; then
-        echo "Changes detected on branch '$BRANCH'! Performing action..." >> "$LOG_DIR/log.txt" 2>&1
+        echo "Changes detected on branch '$BRANCH'! Performing action..." >> "$LOG_DIR/network-skripts-log.txt" 2>&1
         
         # Update the last commit hash
         echo "$CURRENT_COMMIT" > "$LAST_COMMIT_FILE"
 		LAST_COMMIT=$(git rev-parse HEAD)
 
         # Pull changes for the specific branch
-        git pull origin "$BRANCH" >> "$LOG_DIR/log.txt" 2>&1
+        git pull origin "$BRANCH" >> "$LOG_DIR/network-skripts-log.txt" 2>&1
         
     else
-        echo "No changes detected on branch '$BRANCH'." >> "$LOG_DIR/log.txt" 2>&1
+        echo "No changes detected on branch '$BRANCH'." >> "$LOG_DIR/network-skripts-log.txt" 2>&1
     fi
 
     # Sleep for a specified interval (e.g., 60 seconds)
